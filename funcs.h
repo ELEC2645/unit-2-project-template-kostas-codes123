45da684e62c9c78904ae153566dfd4f86e2b3f7a
kostas-codes123 <ll23km@leeds.ac.uk>
2025-12-23 23:54:46 +0000

    Update funcs.h
    the updated version

--- funcs.h
+++ funcs.h
@@ -1,9 +1,19 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
-void menu_item_1(void);
-void menu_item_2(void);
-void menu_item_3(void);
-void menu_item_4(void);
+// These are the shared constants used by funcs.c and main.c
+#define MAX_LENGTH 128
+#define MIN_LENGTH 8
+
+// Public Application Programming Interface implemented in funcs.c 
+void read_password_from_user(char password[], int max_length);
+
+int  check_minimum_length(const char password[]);
+int  check_required_character_types(const char password[]);
+int  has_simple_sequence(const char password[]);
+int  is_common_password_top20(const char password[]);
+int  compute_password_score(const char password[]);
+
+void print_password_feedback(const char password[], int score);
 
 #endif
